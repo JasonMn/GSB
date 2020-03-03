573dc2570a2ad4a1f8d52f2bffbafa6343f3e437
FabDuth <51161754+FabDuth@users.noreply.github.com>
2020-03-03 11:50:48 +0100

    Add files via upload

--- nouveau_site/Fichesaisie.php
+++ nouveau_site/Fichesaisie.php
@@ -1,6 +1,10 @@
 <?php
-session_start();
+if (session_start())
+{
+  echo $_SESSION['login'];
+}
 ?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -23,43 +27,23 @@ include("identifiants.php");
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav mr-auto">
       
-      <?php 
-      if(empty($_SESSION)){
-        echo ('<li class="nav-item">
-        <a class="nav-link" href="connexion.php">Connexion</a>
-        </li>');}
-      else {
-        echo ('<li class="nav-item">
-        <a class="nav-link" href="deconnexion.php">Déconnexion</a>
-        </li>');};
-      
-      
-      
-      ?>
-      
-      <li class="nav-item">
+     
+     
+      <li class="nav-item active">
         <a class="nav-link" href="Fichesaisie.php">Fiche frais</a>
       </li>
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Remboursement<span class="sr-only">(current)</span></a>
+      <li class="nav-item">
+        <a class="nav-link" href="suiviremb.php">Remboursement</a>
       </li>
       <li class="nav-item">
         <a class="nav-link" href="validation.php">Validation</a>
       </li>
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Dropdown
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="#">Action</a>
-          <a class="dropdown-item" href="#">Another action</a>
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#">Something else here</a>
-        </div>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-      </li>
+       <li class="nav-item">
+        <a class="nav-link" href="deconnexion.php">Déconnexion</a>
+        </li>
+      
+       
+    
     </ul>
     
   </div>
@@ -69,23 +53,47 @@ include("identifiants.php");
 <?php
 $titre="Connexion";
 include("identifiants.php");
-if (!isset($_POST['mois'])){
+$user = $_SESSION['login'];
+
+$query=$db->prepare(
+  'SELECT visiteur.id AS id,
+  visiteur.login AS login
+  FROM visiteur 
+  WHERE visiteur.login = :user ');
+  $query->bindValue(':user', $user, PDO::PARAM_STR);
+  $query->execute();
+if ($data=$query->fetch())
+{
+
 echo '<form method="post" action="Fichesaisie.php">
 <fieldset>
 <p class="pe"> PERIODE DENGAGEMENT </p>
 <legend> Saisie </legend>
 <p> 
 <label class="txtmois" for="mois">Mois (2 chiffres) :</label><input class="mois" name="mois" type="text" id="mois" /><br />
-<label class="txtannee for="txtAnnee">Année (4 chiffres) :</label><input class="annee" name="Annee" type="text" id="Annee" />
+<label class="txtannee for="txtAnnee">Année (4 chiffres) :</label><input class="annee" name="annee" type="text" id="Annee" />
 </p>
-</fieldset></html>';}
+</fieldset></html>';
+if(empty($_POST['mois']))
+{
+    echo '<p>Veuillez entrer un mois</p>';
+}
 else
 {
-  $req = $db->prepare("INSERT INTO fichefrais(idVisiteur, mois, idEtat) VALUES(?, ?, 'CR')");
-  $req->execute(array($_SESSION['id'], $_POST['mois']));
+  $req = $db->prepare("INSERT INTO fichefrais(idVisiteur, mois, annee) 
+                      VALUES(?, ?, ?)");
+ if ($req->execute(array($data['id'], $_POST['mois'], $_POST['annee'])))
+ {
+   echo '<p>Data saved</p>';
+ }
+ else
+ {
+   echo '<p>Not saved</p>';
+ }
+}
 }
 
-if (!isset($_POST['mois'])){
+
 echo '<form method="post" action="Fichesaisie.php">
 <fieldset>
 <legend> Frais au forfait  </legend>
@@ -95,13 +103,25 @@ echo '<form method="post" action="Fichesaisie.php">
 <label class="labfrais" for="txtEtape">Étape :</label><input class="infrais3" name="Etape" type="text" id="Etape" /><br />
 <label class="labfrais" for="txtkm">Km :</label><input class="infrais4" name="Km" type="text" id="Km" /><br />
 </p>
-</fieldset></html>';}
+</fieldset></html>';
+if(empty($_POST['Repas']))
+{
+    echo '<p>Veuillez entrer un mois</p>';
+}
 else
 {
-  $req = $db->prepare("INSERT INTO fraisforfait(id, , idEtat) VALUES(?, ?, 'CR')");
-  $req->execute(array($_SESSION['id'], $_POST['mois']));
+  $req2 = $db->prepare("INSERT INTO frais_forfait(repas_midi, nuitee, etape, km) VALUES(?, ?, ?, ?)");
+  if ($req2->execute(array($_POST['Repas'], $_POST['Nuitee'], $_POST['Etape'], $_POST['Km'])))
+  {
+    echo '<p>Data saved</p>';
+  }
+  else
+  {
+    echo '<p>Not saved</p>';
+  }
 }
 
+echo '<form method="post" action="Fichesaisie.php">
 <fieldset>
 <legend> Hors forfait  </legend>
 <p>
@@ -109,21 +129,57 @@ else
 <label class="labhf" for="txtLibellé">Libellé :</label><input class="inhf2" name="Libellé" type="text" id="Libellé" /><br />
 <label class="labhf" for="txtQté">Qté :</label><input class="inhf3" name="Qté" type="text" id="Qté" /><br />
 </p>
-</fieldset>
+</fieldset>';
+if(empty($_POST['Date']))
+{
+  echo '<p>Veuillez entrer une date</p>';
+}
+else
+{
+$req3 = $db->prepare("INSERT INTO frais_hf (date, libelle, Qte) VALUES(?, ?, ?)");
+if ($req3->execute(array($_POST['Date'], $_POST['Libellé'], $_POST['Qté'])))
+{
+  echo '<p>Data saved</p>';
+}
+else
+{
+  echo '<p>Not saved</p>';
+}
+}
 
+echo'
 <fieldset>
 <legend> Hors classification </legend>
 <p>
 <label class="labhf" for="txtNbrJust">Nombre justificatifs :</label><input class="inhc1" name="NbrJust" type="Repas" id="NbrJust" /><br />
 <label class="labhf" for="txtMonTot">Montant total :</label><input class="inhc2" name="MonTot" type="text" id="MonTot" /><br />
 </p>
-</fieldset>
-<div >
+</fieldset>';
+if(empty($_POST['NbrJust']))
+{
+  echo '<p>Veuillez entrer une date</p>';
+}
+else
+{
+$req4 = $db->prepare("INSERT INTO horsclassification (nbr_just, mont) VALUES(?, ?)");
+if ($req4->execute(array($_POST['NbrJust'], $_POST['MonTot'])))
+{
+  echo '<p>Data saved</p>';
+}
+else
+{
+  echo '<p>Not saved</p>';
+}
+}
+
+
+echo '
+<div>
 <p><input class="effacer" type="submit" value="Effacer" /></p>
 </div>
-<div >
+<div>
 <p><input class="effacer" type="submit" value="Valider" /></p>
 </div>
 </body>
-</html>'
+</html>';
 ?>
